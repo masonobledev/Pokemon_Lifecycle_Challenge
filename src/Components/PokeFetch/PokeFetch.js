51cb4b371d3050f30298732758b28a3169667e05
Patrick Cook <patrickcook@Patricks-MacBook-Air.local>
2021-08-30 12:11:03 -0400

    assignment with notes

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -14,8 +14,11 @@ class PokeFetch extends Component {
 
   fetchPokemon() {
     let min = Math.ceil(1);
+    //The math.ceil() function always rounds a number up to the next largest integer.
     let max = Math.floor(152);
+    //The math.floor() function returns the largest integer less than or equal to a given number.
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
+    //The math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1) with approximately uniform distribution over that range.
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
       method: 'GET'
     }).then(res => res.json())
